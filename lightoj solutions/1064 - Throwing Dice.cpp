8c8f1e351bf75d1c28e3774cc223d8b91752b816
Abdullah Al Masud Tushar <rctushar07@gmail.com>
2016-03-20 00:21:10 +0600

    Added files via upload

---
+++ lightoj solutions/1064 - Throwing Dice.cpp
@@ -0,0 +1,180 @@
+/*
+   ~~~ BISMILLAH HIR RAHMANIR RAHIM ~~~
+   Problem Name: 1064 - Throwing Dice
+       Platform: LightOj
+     Algorithom: Probabilistic DP
+  Author's Name: RC Tushar
+
+
+                 শুনেছি তোমার ভীষণ রাগ ?
+                              সে কি রাগ , নাকি
+                                     মগজ কোণে পচন ধরা
+                 পুরনো কোডের বাগ ?
+
+
+         ~~~~~ MY DEDICATION TO SOME FRIENDS ~~~~~
+
+   Kim: Whenever you start to believe  yourself, people also start to believe in you.
+  Oshy: Every programming contest is like a new life.To do better in every life you must dedicate your real life.
+ Elise: Keep studying and the improvement will come. it's a slow process but each step is rewarding.
+
+
+ ~~~     Never Lose Hope     ~~~
+*/
+
+
+#include<bits/stdc++.h>
+#define pi 2*acos(0.0)
+#define PS system("pause")
+#define siter(n,T1)  for(set<T1>::iterator it=n.begin();it!=n.end();it++)
+#define miter(n,T1,T2)  for(map<T1,T2>::iterator it=n.begin();it!=n.end();it++)
+#define meter(n,T1,T2)  for(map<T1,T2>::iterator it=n.end();it!=n.begin();it--)
+#define FOR(s,e,inc) for(int i=s;i<=e;i+=inc)
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define Sfor(n) for(int i=1;i<=n;i++)
+#define inf (1<<30)
+#define pb push_back
+#define ppb pop_back
+#define F first
+#define S second
+#define sz(x) ((int)x.size())
+#define eps 1e-9
+#define gcd(x,y) __gcd(x,y)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define on(x,w)  x=x|(1<<w)
+#define check(x,w) (x&(1<<w))==(1<<w)?true:false
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define s(n) scanf("%d",&n)
+#define sl(n) scanf("%lld",&n)
+#define pf printf
+#define ll long long int
+#define ull unsigned long long int
+#define MOD 1000000007
+#define sqr(x) (( (x)* (x))%MOD)
+#define cube(x)   ( (sqr(x)*(x))%MOD)
+#define bit_cnt(x)   __builtin_popcount(x)
+#define maxall(v) *max_element(all(v))
+#define minall(v) *min_element(all(v))
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+#define btz(a)   __builtin_ctz(a)
+#define Mems(p,n) memset(p, n, sizeof(p))
+#define BOUNDARY(i,j,Row,Col) ((i >= 0 && i < Row) && (j >= 0 && j < Col))
+#define ubound(d,val)  int (upper_bound (d.begin(), d.end(), val) - d.begin())
+#define lbound(d,val)  int (lower_bound (d.begin(), d.end(), val) - d.begin())
+
+using namespace std;
+
+//int dx[]= {1,0,-1,0};
+//int dy[]= {0,1,0,-1};                        //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};     //8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};   //Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1};              //Hexagonal Direction
+//freopen("in.txt","r",stdin);
+//freopen("out.txt","w",stdout);
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ll,bool> pib;
+typedef pair<double,double> pdd;
+typedef pair<int,double> pid;
+typedef pair< int, pii> ipii;
+
+
+//TEMPLATES
+template<class T>
+inline bool fs(T &x)
+{
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9')
+    {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())
+        x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+
+
+int sat(int n,int pos)
+{
+    return n=n | (1<<pos);
+}
+int reset(int N,int pos)
+{
+    return N= N & ~(1<<pos);
+}
+bool cak(int n,int pos)
+{
+    return (bool)(n&(1<<pos));
+}
+
+ll binPow(ll a, ll q, ll mud)
+{
+    a %= mud;
+    if (q == 0) return 1;
+    return ((q % 2 == 1 ? a : 1) * binPow(a * a, q / 2, mud)) % mud;
+}
+
+#define M 100000
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~ Code Starting Point ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ull dp[30][160];
+ull pw[27];
+
+void prec()
+{
+    pw[0]=1;
+    for(ull i=1;i<=25;i++)
+    pw[i]=pw[i-1]*6;
+
+    dp[0][0]=1;
+
+    for(int i=1;i<=25;i++)
+        for(int j=i;j<=i*6;j++)
+            for(int k=1;k<=6;k++)
+            if(j-k>=0)
+            dp[i][j]+=dp[i-1][j-k];
+
+   for(int i=1;i<=25;i++)
+        for(int j=1;j<=i*6;j++)
+        dp[i][j]+=dp[i][j-1];
+}
+
+
+int main()
+{
+    prec();
+
+    ull nom,dinom,gc;
+    int n,k,test;
+    fs(test);
+    Sfor(test)
+    {
+        fs(n);
+        fs(k);
+        nom=dp[n][n*6]-dp[n][k-1];
+        gc=gcd(pw[n],nom);
+        nom/=gc;
+        dinom=pw[n]/gc;
+        if(nom==0||k>n*6)
+            pf("Case %d: 0\n",i);
+        else if(nom==dinom)
+            pf("Case %d: 1\n",i);
+        else
+            pf("Case %d: %llu/%llu\n",i,nom,dinom);
+    }
+    return 0;
+}
+
+
+
+
+
