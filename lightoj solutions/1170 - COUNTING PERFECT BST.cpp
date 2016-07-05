e33b5111e5182c342da1eecdde0a5e9585f090c2
Abdullah Al Masud Tushar <rctushar07@gmail.com>
2016-07-05 23:57:56 +0600

    Add files via upload

---
+++ lightoj solutions/1170 - COUNTING PERFECT BST.cpp
@@ -0,0 +1,208 @@
+/*
+   ~~~ BISMILLAH HIR RAHMANIR RAHIM ~~~
+   Problem Name: 1170 - COUNTING PERFECT BST
+       Platform: LightOj
+      Algorithm: Binary Search+ DP
+  Author's Name: RC Tushar
+
+
+
+         ~~~~~ MY DEDICATION TO SOME FRIENDS ~~~~~
+   Kim: Whenever you start to believe  yourself, people also start to believe in you.
+  Oshy: Every programming contest is like a new life.To do better in every life you must dedicate your real life.
+ Elise: Keep studying and the improvement will come. it's a slow process but each step is rewarding.
+
+
+ ~~~     Never Lose Hope     ~~~
+*/
+
+
+#include<bits/stdc++.h>
+#define pi 2*acos(0.0)
+#define PS system("pause")
+#define siter(n,T1)  for(set<T1>::iterator it=n.begin();it!=n.end();it++)
+#define miter(n,T1,T2)  for(map<T1,T2>::iterator it=n.begin();it!=n.end();it++)
+#define meter(n,T1,T2)  for(map<T1,T2>::iterator it=n.end();it!=n.begin();it--)
+#define FOR(s,e,inc) for(int i=s;i<=e;i+=inc)
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define Sfor(n) for(int i=1;i<=n;i++)
+#define pb push_back
+#define ppb pop_back
+#define F first
+#define S second
+#define sz(x) ((int)x.size())
+#define eps 1e-9
+#define gcd(x,y) __gcd(x,y)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define on(x,w)  x=x|(1<<w)
+#define check(x,w) (x&(1<<w))==(1<<w)?true:false
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define s(n) scanf("%d",&n)
+#define sl(n) scanf("%lld",&n)
+#define pf printf
+#define ll long long int
+#define ull unsigned long long int
+#define MOD 100000007
+#define inf (1<<29)
+#define sqr(x) (( (x)* (x))%MOD)
+#define cube(x)   ( (sqr(x)*(x))%MOD)
+#define maxall(v) *max_element(all(v))
+#define minall(v) *min_element(all(v))
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+#define bit_cnt(x)   __builtin_popcount(x)
+#define btz(a)   __builtin_ctz(a)
+#define Mems(p,n) memset(p, n, sizeof(p))
+#define BOUNDARY(i,j,Row,Col) ((i >= 0 && i < Row) && (j >= 0 && j < Col))
+#define ubound(d,val)  int (upper_bound (d.begin(), d.end(), val) - d.begin())
+#define lbound(d,val)  int (lower_bound (d.begin(), d.end(), val) - d.begin())
+
+using namespace std;
+
+//int dx[]= {1,0,-1,0};
+//int dy[]= {0,1,0,-1};                        //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};     //8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};   //Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1};              //Hexagonal Direction
+//freopen("in.txt","r",stdin);
+//freopen("out.txt","w",stdout);
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ll,bool> pib;
+typedef pair<double,double> pdd;
+typedef pair<int,double> pid;
+typedef pair< int, pii> ipii;
+typedef pair< int, string> pis;
+typedef pair< string,int> psi;
+
+//TEMPLATES
+template<class T>
+inline bool fs(T &x)
+{
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9')
+    {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())
+        x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+
+
+int sat(int n,int pos)
+{
+    return n=n | (1<<pos);
+}
+int reset(int N,int pos)
+{
+    return N= N & ~(1<<pos);
+}
+bool cak(int n,int pos)
+{
+    return (bool)(n&(1<<pos));
+}
+
+ll binPow(ll a, ll q, ll mud)
+{
+    a %= mud;
+    if (q == 0) return 1;
+    return ((q % 2 == 1 ? a : 1) * binPow(a * a, q / 2, mud)) % mud;
+}
+
+pll ex_clid(ll a,ll b)
+{
+    if(b==0)
+        return pll(1,0);
+    pll d=ex_clid(b,a%b);
+    return pll(d.S,d.F-d.S*(a/b));
+}
+
+ll mod_inv(ll n,ll md)
+{
+    pll d=ex_clid(n,md);
+    return ((d.F%md)+md)%md;
+}
+
+template<class T>
+double dif(T &a,T& b)
+{
+    double ans=(a.F-b.F)*(a.F-b.F)+(a.S-b.S)*(a.S-b.S);
+    return sqrt(ans);
+}
+
+#define M  100000
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~ Code Starting Point ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+ll dp[M+5];
+map<ll,bool>mark;
+int ln;
+vector<ll> ara;
+
+void prec()
+{
+    for(ll i=2;i<=M;i++)
+        if(!mark[i])
+            for(ll j=i*i;j<=10000000000;j*=i)
+            {
+                ara.pb(j);
+                mark[j]=true;
+            }
+
+    sort(all(ara));
+    ln=ara.size();
+}
+
+ll solv(int i)
+{
+    if(i==0||i==1)
+        return 1;
+    if(dp[i]!=-1)
+        return dp[i];
+
+    ll sum=0;
+    for(int j=0; j<i; j++)
+    {
+        sum+=(solv(j)%MOD*solv(i-j-1)%MOD);
+        sum%=MOD;
+    }
+
+    return dp[i]=sum;
+}
+
+int main()
+{
+    Mems(dp,-1);
+    prec();
+    ll a,b;
+    int tt;
+    fs(tt);
+    for(int kk=1;kk<=tt;kk++)
+    {
+        fs(a);
+        fs(b);
+        int pa=lower_bound(all(ara),a)-ara.begin();
+        int pb=upper_bound(all(ara),b)-ara.begin();
+        if(pa==pb)
+        pf("Case %d: 0\n",kk);
+        else
+        pf("Case %d: %lld\n",kk,solv(pb-pa));
+    }
+
+    return 0;
+}
+
+
+
+
+
+
