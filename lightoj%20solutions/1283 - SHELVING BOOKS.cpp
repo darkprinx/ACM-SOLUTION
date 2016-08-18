0a5e53306607c7ba08a93d2dd067a8ff470d2937
Abdullah Al Masud Tushar <rctushar07@gmail.com>
2016-08-18 16:57:14 +0600

    Add files via upload

---
+++ lightoj%20solutions/1283 - SHELVING BOOKS.cpp
@@ -0,0 +1,191 @@
+/*
+
+   ~~~ BISMILLAH HIR RAHMANIR RAHIM ~~~
+
+   Problem Name: 1283 - SHELVING BOOKS
+       Platform: LightOj
+      Algorithm: DP
+  Author's Name: RC Tushar
+
+
+      ~~~     Never Lose Hope     ~~~
+*/
+
+
+#include<bits/stdc++.h>
+#define pi 2*acos(0.0)
+#define PS system("pause")
+#define siter(n,T1)  for(set<T1>::iterator it=n.begin();it!=n.end();it++)
+#define miter(n,T1,T2)  for(map<T1,T2>::iterator it=n.begin();it!=n.end();it++)
+#define meter(n,T1,T2)  for(map<T1,T2>::iterator it=n.end();it!=n.begin();it--)
+#define FOR(s,e,inc) for(int i=s;i<=e;i+=inc)
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define Sfor(n) for(int i=1;i<=n;i++)
+#define pb push_back
+#define ppb pop_back
+#define F first
+#define S second
+#define sz(x) ((int)x.size())
+#define eps 1e-11
+#define gcd(x,y) __gcd(x,y)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define on(x,w)  x=x|(1<<w)
+#define check(x,w) (x&(1<<w))==(1<<w)?true:false
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define s(n) scanf("%d",&n)
+#define sl(n) scanf("%lld",&n)
+#define pf printf
+#define ll long long int
+#define ull unsigned long long int
+#define MOD 1000000007
+#define inf (1<<29)
+#define sqr(x) (( (x)* (x))%MOD)
+#define cube(x)   ( (sqr(x)*(x))%MOD)
+#define maxall(v) *max_element(all(v))
+#define minall(v) *min_element(all(v))
+#define max3(a,b,c) max(a,max(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+#define bit_cnt(x)   __builtin_popcount(x)
+#define btz(a)   __builtin_ctz(a)
+#define Mems(p,n) memset(p, n, sizeof(p))
+#define BOUNDARY(i,j,Row,Col) ((i >= 0 && i < Row) && (j >= 0 && j < Col))
+#define ubound(d,val)  int (upper_bound (d.begin(), d.end(), val) - d.begin())
+#define lbound(d,val)  int (lower_bound (d.begin(), d.end(), val) - d.begin())
+
+using namespace std;
+
+//int dx[]= {1,0,-1,0};
+//int dy[]= {0,1,0,-1};                        //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};     //8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};   //Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1};              //Hexagonal Direction
+//freopen("in.txt","r",stdin);
+//freopen("out.txt","w",stdout);
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ll,bool> pib;
+typedef pair<double,double> pdd;
+typedef pair<int,double> pid;
+typedef pair< int, pii> ipii;
+typedef pair< int, string> pis;
+typedef pair< string,int> psi;
+
+//TEMPLATES
+template<class T>
+inline bool fs(T &x)
+{
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9')
+    {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())
+        x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+
+
+int sat(int n,int pos)
+{
+    return n=n | (1<<pos);
+}
+int reset(int N,int pos)
+{
+    return N= N & ~(1<<pos);
+}
+bool cak(int n,int pos)
+{
+    return (bool)(n&(1<<pos));
+}
+
+ll binPow(ll a, ll q, ll mud)
+{
+    a %= mud;
+    if (q == 0) return 1;
+    return ((q % 2 == 1 ? a : 1) * binPow(a * a, q / 2, mud)) % mud;
+}
+
+pll ex_clid(ll a,ll b)
+{
+    if(b==0)
+        return pll(1,0);
+    pll d=ex_clid(b,a%b);
+    return pll(d.S,d.F-d.S*(a/b));
+}
+
+ll mod_inv(ll n,ll md)
+{
+    pll d=ex_clid(n,md);
+    return ((d.F%md)+md)%md;
+}
+
+
+template<class T>
+double dif(T &a,T& b)
+{
+    double ans=(a.F-b.F)*(a.F-b.F)+(a.S-b.S)*(a.S-b.S);
+    return sqrt(ans);
+}
+
+#define M   5000
+#define mod  1000000007
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~ Code Starting Point ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int n;
+int ara[105];
+int dp[105][105][105];
+
+int solv(int pos,int l,int r)
+{
+    if(pos==n+1)
+        return 0;
+    if(dp[pos][l][r]!=-1)
+        return dp[pos][l][r];
+
+    int x1,x2,x3;
+    x1=x2=x3=0;
+
+    if(ara[l]<=ara[pos]&&ara[pos]<=ara[r])
+    {
+        x1=solv(pos+1,pos,r)+1;
+        x2=solv(pos+1,l,pos)+1;
+    }
+    x3=solv(pos+1,l,r);
+    return dp[pos][l][r]=max3(x1,x2,x3);
+}
+int main()
+{
+    int a,b,tt;
+    fs(tt);
+
+    for(int kk=1; kk<=tt; kk++)
+    {
+        fs(n);
+
+        ara[0]=0;
+        ara[n+1]=inf;
+        for(int i=1;i<=n;i++)
+        fs(ara[i]);
+
+        Mems(dp,-1);
+        pf("Case %d: %d\n",kk,solv(1,0,n+1));
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
